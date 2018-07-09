9549c803d70c925ae20b6ae64143b73f83f59e92
Hubben, Nicklaus (BOS-GEN) <NHubben@wearegenuine.com>
2018-07-09 11:51:55 -0400

    distort inherits GameObject transform position

--- Assets/Editor/DistortPlane_Editor.cs
+++ Assets/Editor/DistortPlane_Editor.cs
@@ -7,12 +7,14 @@ public class DistortPlane_Editor : Editor
     protected virtual void OnSceneGUI()
     { 
         DistortPlane plane = (DistortPlane)target;
+        Vector3 position = plane.gameObject.transform.localPosition;
         Vector3[] corners = plane.Corners;
  
         EditorGUI.BeginChangeCheck();
         for (int i = 0; i < corners.Length; i++)
         { 
-            corners[i] = Handles.PositionHandle(corners[i], Quaternion.identity);
+            corners[i] = Handles.PositionHandle(position + corners[i], Quaternion.identity);
+            corners[i] -= position;
         }
 
         if (EditorGUI.EndChangeCheck())
